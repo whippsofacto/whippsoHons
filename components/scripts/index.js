ef8fdd19116620e8ae410fceed6f15d15e9c004f
Stephen Whipp <stephen@whippy.co.uk>
2017-03-22 06:24:24 +0000

    007 -- Changed the file structure to only output one jsFile, deleted myscripts.js from component folder Added the aframe-event-set-component to project.

--- components/scripts/index.js
+++ components/scripts/index.js
@@ -6,3 +6,24 @@ var aframe = require ('aframe');
 var extras = require('aframe-extras');
 //gradient-sky
 var gradientSky = require('aframe-gradient-sky');
+//event set-image
+var eventSet = require('aframe-event-set-component');
+
+
+console.log("hello world");
+//register the use of the extras
+extras.registerAll();
+
+AFRAME.registerComponent('log', {
+  schema: {type: 'string'},
+	init: function () {
+    var stringToLog = this.data;
+    console.log(stringToLog);
+
+		var title = $( "#testBox" )
+		.attr({
+			color:"blue",
+			position: "0 0 -9"
+		});
+  }
+});
