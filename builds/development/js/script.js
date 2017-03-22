9b234d48e47f58857f17b9d82b2af96061bafeb4
Stephen Whipp <stephen@whippy.co.uk>
2017-03-22 05:12:17 +0000

    006 Testing with register.component and javascript call backs

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -11,30 +11,12 @@ var gradientSky = require('aframe-gradient-sky');
 console.log("hello world");
 //register the use of the extras
 extras.registerAll();
-
-// <a-ocean-plane width="100" height="100"></a-ocean-plane>
-AFRAME.registerPrimitive('a-ocean-plane', {
-	defaultComponents: {
-		geometry: {
-			primitive: 'plane',
-			height: 10000,
-			width: 10000
-		},
-		rotation: '-90 0 0',
-		material: {
-			shader: 'standard',
-			color: '#8ab39f',
-			metalness: 1,
-			roughness: 0.2,
-			opacity: 0.8
-		}
-	},
-	mappings: {
-		width: 'geometry.width',
-		height: 'geometry.depth',
-		color: 'material.color',
-		opacity: 'material.opacity'
-	}
+AFRAME.registerComponent('log', {
+  schema: {type: 'string'},
+  init: function () {
+    var stringToLog = this.data;
+    console.log(stringToLog);
+  }
 });
 
 },{"aframe":49,"aframe-extras":2,"aframe-gradient-sky":37,"jquery":107}],2:[function(require,module,exports){
--- components/scripts/myScripts.js
+++ components/scripts/myScripts.js
@@ -1,28 +1,10 @@
 console.log("hello world");
 //register the use of the extras
 extras.registerAll();
-
-// <a-ocean-plane width="100" height="100"></a-ocean-plane>
-AFRAME.registerPrimitive('a-ocean-plane', {
-	defaultComponents: {
-		geometry: {
-			primitive: 'plane',
-			height: 10000,
-			width: 10000
-		},
-		rotation: '-90 0 0',
-		material: {
-			shader: 'standard',
-			color: '#8ab39f',
-			metalness: 1,
-			roughness: 0.2,
-			opacity: 0.8
-		}
-	},
-	mappings: {
-		width: 'geometry.width',
-		height: 'geometry.depth',
-		color: 'material.color',
-		opacity: 'material.opacity'
-	}
+AFRAME.registerComponent('log', {
+  schema: {type: 'string'},
+  init: function () {
+    var stringToLog = this.data;
+    console.log(stringToLog);
+  }
 });
