0862b249ea679c746a96f1673e9be31b891a1f6b
Stephen Whipp <stephen@whippy.co.uk>
2017-03-25 12:59:20 +0000

    014 -- Added attributes with multiple properties to boxes already responding to click events

--- components/scripts/index.js
+++ components/scripts/index.js
@@ -17,14 +17,14 @@ extras.registerAll();
 //*------ Components -----*//
 AFRAME.registerComponent('foo', {
   init: function () {
-
+    /*
     console.log(this.el.sceneEl);  // Reference to the scene element.
     var testBox = this.el.sceneEl.querySelector('#testBox');
     var myBox = this.el.sceneEl.querySelector('#myBox');
     var myBox1 = this.el.sceneEl.querySelector('#myBox1');
     var myBox2 = this.el.sceneEl.querySelector('#myBox2');
 
-    /*
+
     console.log (testBox);
     testBox.onclick = function(){
       myBox.setAttribute('material', 'color', 'orange');
@@ -33,14 +33,19 @@ AFRAME.registerComponent('foo', {
       myBox2.setAttribute('material', 'color', 'red');
     }
     */
-
     $('#testBox').click(function() {
     if ($('#myBox').hasClass('test')) {
         $('#myBox').attr('scale',"0 0 0");
+        $('#myBox1').attr('scale',"1.5 1.5 1.5");
+        $('#myBox2').attr({position:"-0.91 0.5 -14.5",
+                           scale: "3 3 3",
+                           geometry: "primitive: cylinder"});
         $('#myBox').removeClass('test');
     } else {
       ($('#myBox').attr('scale',"1 1 1"));
       ($('#myBox').addClass('test'));
+      $('#myBox1').attr('scale',"1 1 1");
+      $('#myBox2').attr('position',"-4.0 1.26 -4.7");
     }
 });
 
