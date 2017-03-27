5efdad971fdd34b995e9a4d6071a726c7164daa1
Stephen Whipp <stephen@whippy.co.uk>
2017-03-27 16:07:41 +0100

    016 -- Added speech bubble element, animated and created conversation dialogue tree

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -22,6 +22,8 @@ AFRAME.registerComponent('foo', {
     //Variables ---------------------------- //
     var scene = $('a-scene');
     var entity = $(document.createElement('a-entity'));
+    var yesBtn = $(document.createElement('a-plane'));
+    var noBtn = $(document.createElement('a-plane'));
 
     //--- Animations Testing ---//
     $('#testBox').click(function() {
@@ -51,11 +53,52 @@ AFRAME.registerComponent('foo', {
     $('#myBox').one("click", function() {
       console.log("clicked");
       scene.append(entity);
-      entity.attr({      id: "cone",
-                         position:"-0.91 2.5 -14.5",
-                         geometry: "primitive: cone",
-                         animation__conescale:"property: scale; dir: normal; dur: 5000; easing: easeInSine; loop: false; from: 0 0 0; to: 1 1 1"});
+      entity.attr({    "obj-model": "obj: #speech-obj; mtl: #speech-mtl",
+                       position:"-1.36 2.49 -2.37",
+                       scale:"0 0 0",
+                       rotation:" 90 25 0",
+                       animation__conescale:"property: scale; dir: normal; dur: 200; easing: easeInSine; loop: false; from: 0 0 0; to: 0.4 0.4 0.4"});
+     scene.append(yesBtn);
+     yesBtn.attr({
+         id: "yesBtn",
+         scale: "0.5 0.5 0.5",
+         rotation:" 0 25 0",
+         position:"-0.52 1.66 -2.78",
+         material: "color: #1BEDC3",
+         animation__conescale:"property: scale; dir: normal; dur: 200; easing: easeInSine; loop: false; from: 0 0 0; to: 0.4 0.4 0.4"});
+     yesBtn.one("click",function(){
+          console.log("yes!");
+          yesBtn.attr({animation__scale:"property: scale; dir: normal; dur: 2000; easing: easeInSine; loop: false; to: 0 0 0",
+                       animation__rotate:"property: rotation; dir: normal; dur: 300; easing: easeInOutSine; loop:true; from: 0 0 0; to: 0 0 1080"
+              });
+          setTimeout(function(){
+                      $(yesBtn).remove();
+                      alert("removed");
+                    },2200);
+          });
+     scene.append(noBtn);
+     noBtn.attr({
+         id: "noBtn",
+         scale: "0.5 0.5 0.5",
+         rotation:" 0 25 0",
+         position:"-1.91 1.66 -2.10",
+         animation__conescale:"property: scale; dir: normal; dur: 200; easing: easeInSine; loop: false; from: 0 0 0; to: 0.4 0.4 0.4"});
+     noBtn.one("click",function(){
+          console.log("no!");
+          setTimeout(function(){
+                      $(noBtn).remove();
+                      alert("removed");
+                    },300);
+        });    
     });
+
+
+
+
+
+
+
+
 
 
 
