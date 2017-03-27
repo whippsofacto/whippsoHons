cd4849354d1ae1c1cd9c5a217eeb5bd1db8785f5
Stephen Whipp <stephen@whippy.co.uk>
2017-03-27 12:42:54 +0100

    015 -- Added more click events, append new elements

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -18,37 +18,48 @@ extras.registerAll();
 //*------ Components -----*//
 AFRAME.registerComponent('foo', {
   init: function () {
-    /*
-    console.log(this.el.sceneEl);  // Reference to the scene element.
-    var testBox = this.el.sceneEl.querySelector('#testBox');
-    var myBox = this.el.sceneEl.querySelector('#myBox');
-    var myBox1 = this.el.sceneEl.querySelector('#myBox1');
-    var myBox2 = this.el.sceneEl.querySelector('#myBox2');
 
+    //Variables ---------------------------- //
+    var scene = $('a-scene');
+    var entity = $(document.createElement('a-entity'));
 
-    console.log (testBox);
-    testBox.onclick = function(){
-      myBox.setAttribute('material', 'color', 'orange');
-      myBox.setAttribute('class', 'test');
-      myBox1.setAttribute('material', 'color', 'green');
-      myBox2.setAttribute('material', 'color', 'red');
-    }
-    */
+    //--- Animations Testing ---//
     $('#testBox').click(function() {
     if ($('#myBox').hasClass('test')) {
-        $('#myBox').attr('scale',"0 0 0");
+        $('#myBox').attr({scale:"0 0 0",
+                          visible:"false"});
         $('#myBox1').attr('scale',"1.5 1.5 1.5");
         $('#myBox2').attr({position:"-0.91 0.5 -14.5",
                            scale: "3 3 3",
-                           geometry: "primitive: cylinder"});
+                           geometry: "primitive: cylinder",
+                           animation__thing:"property: scale; dir: normal; dur: 5000; easing: easeInSine; loop: false; to: 4 4 4"});
         $('#myBox').removeClass('test');
+        $('#myBox2').removeAttr('animation__scale');
     } else {
-      ($('#myBox').attr('scale',"1 1 1"));
-      ($('#myBox').addClass('test'));
+      $('#myBox').attr('scale',"1 1 1");
+      $('#myBox').addClass('test');
       $('#myBox1').attr('scale',"1 1 1");
-      $('#myBox2').attr('position',"-4.0 1.26 -4.7");
-    }
-});
+      $('#myBox2').attr( {position:"-4.0 1.26 -4.7",
+                          geometry:"primitive: box",
+                          animation__scale:"property: scale; dir: normal; dur: 5000; easing: easeInSine; loop: false; to: 0 0 0"});
+      $('#myBox2').removeAttr('animation__thing');
+       }
+    });
+
+    //--Adding Elements On Click --//
+    //.one means that the event can only fire once
+    $('#myBox').one("click", function() {
+      console.log("clicked");
+      scene.append(entity);
+      entity.attr({      id: "cone",
+                         position:"-0.91 2.5 -14.5",
+                         geometry: "primitive: cone",
+                         animation__conescale:"property: scale; dir: normal; dur: 5000; easing: easeInSine; loop: false; from: 0 0 0; to: 1 1 1"});
+    });
+
+
+
+
 
 
 
